cf0b134337e178ca6913171c38308e940591f195
diPaoli <dipaolialexandre@gmail.com>
2022-11-10 22:05:26 -0300

    Só organizar main.py. Nenhuma alteração.

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi import status
 from fastapi import HTTPException
-from fastapi import Response
 from fastapi.encoders import jsonable_encoder
 
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 from sqlalchemy.orm import Session
@@ -17,8 +15,6 @@ from sqlalchemy import select
 from models import LeituraModel
 from schemas import LeituraSchema
 from database import engine
-
-
 
 
 app = FastAPI()
@@ -43,13 +39,38 @@ async def get_leituras_by_ap(request: Request, ap_num: int):
         return templates.TemplateResponse("consulta.html", {"request": request, "leituras": lista})
 
 
+@app.get('/lista/{id_leitura}', status_code=status.HTTP_200_OK)
+def get_leitura_by_id(id_leitura: int):
+    with Session(engine) as session:
+        query = select(LeituraModel).filter_by(id=id_leitura)
+        result = session.execute(query).first()
+        return result
+
+
+# TODO: Filtro por data
+@app.get('/lista/data/{data}', status_code=status.HTTP_200_OK)
+def get_leituras_by_date(data: date):
+    with Session(engine) as session:
+        query = select(LeituraModel).filter_by(data=data)
+        result = session.execute(query).all()
+        return result
+
+
+# TODO: Filtro por período
+@app.get('/lista/data/{data_ini}/{data_fin}', status_code=status.HTTP_200_OK)
+def get_leituras_within_period(data_ini, data_fin: date):
+    with Session(engine) as session:
+        query = select(LeituraModel).filter(LeituraModel.data >= data_ini, LeituraModel.data <= data_fin)
+        result = session.execute(query).all()
+        return result
+
+
 @app.get('/leitura')
-async def qr_teste(request: Request):
+async def qrcode_scan(request: Request):
     return templates.TemplateResponse("leitura.html", {"request": request})
 
 
-
-@app.post('/salvar')
+@app.post('/leitura')
 async def post_leitura(request: Request):
     form = jsonable_encoder(await request.form())
     leitura_body = LeituraSchema(
@@ -73,58 +94,19 @@ def grava_leitura(leitura_body: LeituraSchema):
             raise HTTPException(status_code=500, detail=str(exception)) from exception
 
 
-
-
-
-
-
-
-
-
-
-@app.get('/lista/{id_leitura}', status_code=status.HTTP_200_OK)
-def get_leitura_by_id(id_leitura: int):
-    with Session(engine) as session:
-        query = select(LeituraModel).filter_by(id=id_leitura)
-        result = session.execute(query).first()
-        return result
-
-
-
-
-
-@app.get('/lista/data/{data}', status_code=status.HTTP_200_OK)
-def get_leituras_by_date(data: date):
-    with Session(engine) as session:
-        query = select(LeituraModel).filter_by(data=data)
-        result = session.execute(query).all()
-        return result
-
-
-@app.get('/lista/data/{data_ini}/{data_fin}', status_code=status.HTTP_200_OK)
-def get_leituras_within_period(data_ini, data_fin: date):
-    with Session(engine) as session:
-        query = select(LeituraModel).filter(LeituraModel.data >= data_ini, LeituraModel.data <= data_fin)
-        result = session.execute(query).all()
-        return result
-
-
-
-
-
-@app.delete('/leitura/{leitura_id}', status_code=status.HTTP_200_OK)
-def delete_leitura(leitura_id: int):
-    with Session(engine) as session:
-        try:
-            leitura = session.get(LeituraModel, leitura_id)
-            if not leitura:
-                return Response(status_code=status.HTTP_404_NOT_FOUND)
-            session.delete(leitura)
-            session.commit()
-            return Response(status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            raise HTTPException(status_code=500, detail=str(exception)) from exception
-
+# TODO: Se precisar, fazer tela para editar/excluir registros
+# @app.delete('/leitura/{leitura_id}', status_code=status.HTTP_200_OK)
+# def delete_leitura(leitura_id: int):
+#     with Session(engine) as session:
+#         try:
+#             leitura = session.get(LeituraModel, leitura_id)
+#             if not leitura:
+#                 return Response(status_code=status.HTTP_404_NOT_FOUND)
+#             session.delete(leitura)
+#             session.commit()
+#             return Response(status_code=status.HTTP_200_OK)
+#         except Exception as exception:
+#             raise HTTPException(status_code=500, detail=str(exception)) from exception
 
 
 if __name__ == '__main__':
